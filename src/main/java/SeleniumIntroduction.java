91088d9c9962ca4b902875aab8f9f840b134e3e2
Miroslav Todorov <smash.all22@outlook.com>
2023-06-03 19:06:07 +0300

    #MT_Selenium_04_WebDrivers

--- src/main/java/SeleniumIntroduction.java
+++ src/main/java/SeleniumIntroduction.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumIntroduction {
     /**
@@ -18,14 +20,14 @@ public class SeleniumIntroduction {
         also change the path if you are going to use the project from other local machine,
         and you have to add the extension of the used file, with the name of the file.
          */
-        System.setProperty(ConstantParameters.CHROME_KEY_PROPERTY, ConstantParameters.CHROME_DRIVER_PATH);
+//        System.setProperty(ConstantParameters.CHROME_KEY_PROPERTY, ConstantParameters.CHROME_DRIVER_PATH);
 //        System.setProperty(ConstantParameters.FIREFOX_KEY_PROPERTY, ConstantParameters.FIREFOX_DRIVER_PATH);
-//        System.setProperty(ConstantParameters.EDGE_KEY_PROPERTY, ConstantParameters.EDGE_DRIVER_PATH);
+        System.setProperty(ConstantParameters.EDGE_KEY_PROPERTY, ConstantParameters.EDGE_DRIVER_PATH);
 
         //initialize the driver
-        WebDriver driver = new ChromeDriver();
+//        WebDriver driver = new ChromeDriver();
 //        WebDriver driver = new FirefoxDriver();
-//        WebDriver driver = new EdgeDriver();
+        WebDriver driver = new EdgeDriver();
         driver.get(ConstantParameters.URL);//get and open the target URL site
         driver.manage().window().fullscreen(); //browser goes to full screen
         driver.close();//closes the current window, quit method closes all associated windows
